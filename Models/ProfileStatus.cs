fa7169d2b38091ca93707dfd1c1ce66e3d0b77da
Carlos Herrera <juan.cervantes@rappi.com>
2022-04-24 01:32:21 -0500

    EB-79 Migrate from SQL Server to PostgreSQL

--- Models/ProfileStatus.cs
+++ Models/ProfileStatus.cs
@@ -1,19 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EscortBookCustomerConsumer.Models
 {
+    [Table("profile_status", Schema = "public")]
     public class ProfileStatus
     {
         #region snippet_Properties
 
+        [Column("id")]
         public string ID { get; set; } = Guid.NewGuid().ToString();
 
-        public string ProfileID { get; set; }
+        [Column("customer_id")]
+        public string CustomerID { get; set; }
 
+        [Column("profile_status_category_id")]
         public string ProfileStatusCategoryID { get; set; }
 
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        [Column("updated_at")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         #endregion
--- Models/ProfileStatusCategory.cs
+++ Models/ProfileStatusCategory.cs
@@ -1,19 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EscortBookCustomerConsumer.Models
 {
+    [Table("profile_status_category", Schema = "public")]
     public class ProfileStatusCategory
     {
         #region snippet_Properties
 
+        [Column("id")]
         public string ID { get; set; } = Guid.NewGuid().ToString();
 
+        [Column("name")]
         public string Name { get; set; }
 
+        [Column("active")]
         public bool Active { get; set; }
 
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        [Column("updated_at")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         #endregion
